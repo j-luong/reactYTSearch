6f933833feb2255902b99d076ea7a522edab73d5
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-11-18 22:59:00 +0000

    added ability to search for videos via searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,17 +13,16 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={ this.state.term }
-          onChange={ (event) => this.setState({ term: event.target.value }) }
+          onChange={ (event) => this.onInputChange(event.target.value) }
         />
       </div>
     );
   }
 
-  // ES6 syntax on line 13 makes the method below obsolete
-  // <input onChange={ this.onInputChange } />
-  // onInputChange(event) {
-  //   this.setState({ term: event.target.value });
-  // }
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
