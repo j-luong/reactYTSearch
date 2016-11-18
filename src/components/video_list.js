5ed4dd1e4fa68622b8b7cc806f6c4000efc3a4e8
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-11-18 21:37:06 +0000

    retreives list of video and maps each video to video_list_item component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import VideoListItem from './video_list_item';
+
 //functional component takes 'props' as an argument
 const VideoList = (props) => {
+  // get list of videos and store in video items
+  const videoItems = props.videos.map( (video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     // using bootstrap defined in index.html for className
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      { videoItems }
     </ul>
   );
 };
